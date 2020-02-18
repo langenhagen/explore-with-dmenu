1a1dae958da3f4c5fd88d61aad4bc7bc2ff9aab8
Andreas Langenhagen <andreas@langenhagen.cc>
2020-02-18 14:41:41 +0100

    Refactor explore-with-dmenu.sh

--- explore-with-dmenu.sh
+++ explore-with-dmenu.sh
@@ -2,7 +2,7 @@
 # Call dmenu on the given array of choices, effectively acting as a simple file explorer.
 # If the selected choice is a folder, recursively open dmenu with the folder's contents as choices.
 # If the selection is not a folder - or the current folder, denoted as '.' - attempt to open it with
-# xdg-open or a tool configured via .edmrc-file.
+# xdg-open or a tool configured via .edmrc-file and write the selection to a history file.
 # The script can also open a terminal at the selected path.
 #
 # author: andreasl
@@ -31,15 +31,17 @@ define_standard_settings() {
 define_standard_settings
 source "${HOME}/.config/.edmrc" 2>/dev/null
 
+write_selection_to_history_file() {
+    sed -i "\:${selected_path}:d" "$history_file"
+    printf '%s\n' "$selected_path" >> "$history_file"
+    printf '%s\n' "$(tail -n "$max_history_entries" "$history_file")" > "$history_file"
+}
+
 while : ; do
     dmenu_result="$(printf '%s\n' "${choices[@]}" | dmenu -i -p "$selected_path" -l 50)" || exit 1
     if [ "$dmenu_result" = '<open terminal here>' ]; then
-        eval "$open_terminal_command" "\"${selected_path}\""
-
-        sed -i "\:${selected_path}:d" "$history_file"
-        printf '%s\n' "$selected_path" >> "$history_file"
-        printf '%s\n' "$(tail -n "$max_history_entries" "$history_file")" > "$history_file"
-
+        $open_terminal_command "$selected_path"
+        write_selection_to_history_file
         exit 0
     fi
 
@@ -49,12 +51,8 @@ while : ; do
         selected_path="$(realpath "${selected_path}/${dmenu_result}")"
     fi
     if [ -f "$selected_path" ] || [ "$dmenu_result" = '.' ]; then
-        eval "${open_command} \"${selected_path}\""
-
-        sed -i "\:${selected_path}:d" "$history_file"
-        printf '%s\n' "$selected_path" >> "$history_file"
-        printf '%s\n' "$(tail -n "$max_history_entries" "$history_file")" > "$history_file"
-
+        $open_command "$selected_path"
+        write_selection_to_history_file
         exit 0
     elif [ -d "$selected_path" ]; then
         choices=( '<open terminal here>' '.' '..' "$(ls "$selected_path")")
